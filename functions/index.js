8bb5511d7016bc6707de772a72a95769a60d2c18
Technology Advcoates LLC <info@tech-advocates.com>
2020-10-20 19:54:38 -0600

    first commit

---
+++ functions/index.js
@@ -0,0 +1,42 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin')
+const express = require('express')
+const bodyParser = require('body-parser')
+const { region } = require('firebase-functions');
+
+admin.initializeApp()
+
+async function getFacts() {
+
+    var ref
+    try {
+        ref = await admin.firestore().collection('members').get()
+    } catch (error) {
+        functions.logger.error(`There was an error: ${error}`)
+        return
+    }
+    var docs = ref.docs.map(doc => doc.data())
+
+    return docs
+}
+
+const app = express()
+app.set('views', './views')
+app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
+
+app.get('/', async (request, response) => {
+    const facts = await getFacts()
+    console.log(facts)
+    response.render('index', { data: facts })
+})
+
+app.post('/', async (request, response) => {
+    const breakfast = request.body.breakfast
+    console.log(breakfast)
+    response.render('results', { data: breakfast })
+})
+
+exports.app = functions.https.onRequest(app)
